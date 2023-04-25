260ff32db8cf967e49a1c33de763003e17ac7d2f
Sonu Kumar Singh <hi.ersonusingh@gmail.com>
2023-04-25 13:27:20 +0530

    .

---
+++ Fundamentasl.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    // echo "Hello ", "World";
+    // echo "Hello World";
+    // echo "Hello "."World";
+    // print "Hello "."World";
+
+    //comment in php 
+    //single line comments
+    //Multi line comments
+
+    // echo nl2br("My name is Dary \r\n and in Dutch");
+
+    // echo "I live in Netherlands";
+    // echo "I like PRogramming"; 
+
+    // echo "My name is Dary and I am Dutch <br>";
+    // echo "I live in Netherlands <br>";
+    // echo "I like Programming <br>"; 
+
+    // echo "My name is Dary and I am Dutch"."<br>";
+    // echo "I live in Netherlands <br>"."<br>";
+    // echo "I like PRogramming <br>"; 
+
+    // echo "My name is Dary and I am Dutch".$dutch."<br>";
+    // echo "I live in Netherlands"."<br>";
+    // echo "I like Programming <br>"; 
+
+    //Variables : 
+    //is a symbol or name that stands fot a value
+
+    /*
+    $name = "Dary"
+
+    echo "My name is " .$name."<br>";
+
+    echo $name. "is 24 years old" ."<br>";
+
+    //variable names 
+
+    //variable name starts with a letter A-Z a-z,
+    underscore(_), foloowed by ny number of letters, number, or unserscores.
+
+    //Add variables in echo/print
+    //Echo 
+    //Single quotes - Variable named are echoed as it is
+    //Double quotes - variable names are replaced by the variable value
+    */
+    // $age = 24;
+    // echo "My age is {$age}";
+    // echo 'my age is {$myage}';
+    // echo "My age is ".$myage; //concatenation
+
+    //Data types : 
+    //Integer - 
+    //Floating point or Float
+    //String
+    //Booleans
+    //Array
+    //Object
+    //Resource
+    //Null
+
+    // $age = 24;
+    // echo $age;
+
+    // echo "My age is; ". $age;
+
+    // echo "<br>";
+
+    // $price = 10.5;
+    // echo "The price is: ".$price;
+    // echo "<br>";
+
+    // $name = "Dary";
+    // echo "My name is ".$name;
+
+    // $is_allowed = true;
+    // echo $is_allowed;
+
+    // echo "<br";
+    // $x = "Hello World";
+    // $x = null ;
+    // echo $x;
+    // $y;
+
+    //Arrays: 
+    // $cars1 = "BMW";
+    // $cars2 = "Audi";
+    // $cars3 = "Mercedes";
+
+    // $cars = array("BMW", "Audi", "Mercedes");
+
+    // echo $cars[1];
+
+    $myCar = array("Audi", 2015, 75.304);
+
+    var_dump($myCar);
+
+
+    ?>
+    
+</body>
+</html>
